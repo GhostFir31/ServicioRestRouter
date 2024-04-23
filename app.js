6a21d44495865e7ddeded740719173ceee0e1188
GhostFir31 <j3sus0marle@gmail.com>
2024-04-23 16:06:10 -0700

    up

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 
 const activoController = require('./controladores/activoController');
 const responsableController = require('./controladores/responsableController');
